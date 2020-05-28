8841b4777172699f07e2abbfc0b96e95fa0139b8
Paul Saxe <paul@thesaxes.net>
2020-05-28 09:03:38 -0400

    Initial skeleton.

---
+++ seamm_jobserver/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for SEAMM Widgets."""
+
+__author__ = """Paul Saxe"""
+__email__ = 'psaxe@vt.edu'
+__version__ = '0.2.1'
+
+# Bring up the classes so that they appear to be directly in
+# the seamm_jobserver package.
+
+from seamm_jobserver.jobserver import JobServer  # noqa: F401
+
---
+++ seamm_jobserver/jobserver.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""The JobServer for the SEAMM environment.
+
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class JobServer(object):
+
+    def __init__(self):
+        """Initialize the instance
+        """
+        super().__init__()
